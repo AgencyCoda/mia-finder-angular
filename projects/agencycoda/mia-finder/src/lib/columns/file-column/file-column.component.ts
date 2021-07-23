feb6e0a3abc610c23be6816351c917cd1d475e0b
Matias Camiletti <matias.camiletti@gmail.com>
2021-07-23 17:58:45 -0300

    Add icons in table.

--- projects/agencycoda/mia-finder/src/lib/columns/file-column/file-column.component.ts
+++ projects/agencycoda/mia-finder/src/lib/columns/file-column/file-column.component.ts
@@ -1,5 +1,7 @@
+import { StringHelper } from '@agencycoda/mia-core';
 import { BaseColumnComponent } from '@agencycoda/mia-table';
 import { Component, OnInit } from '@angular/core';
+import { MiaFinder } from '../../entities/mia-finder';
 
 @Component({
   selector: 'lib-file-column',
@@ -15,4 +17,12 @@ export class FileColumnComponent extends BaseColumnComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getIconImage(): string {
+    if(this.item.type == MiaFinder.TYPE_FOLDER){
+      return '/assets/icons/file-folder.svg';
+    } else if (this.item.type == MiaFinder.TYPE_LINK) {
+      return '/assets/icons/file-link.svg';
+    }
+    return '/assets/icons/file' + StringHelper.getExtension(this.item.url) + '.svg';
+  }
 }
